fdd50faa9cad522bd37d489ada0e59ec5d67e4ed
bassel97 <bassel.ahmed97@eng-st.cu.edu.eg>
2019-03-15 22:53:44 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+"# RenderEnginePractice" 
+
+This is me practicing making a complete renderer in c++.
---
+++ RenderEnginePractice/RenderEnginePractice.cpp
@@ -0,0 +1,10 @@
+// RenderEnginePractice.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello Render Engine!\n"; 
+
+	return 1;
+}
